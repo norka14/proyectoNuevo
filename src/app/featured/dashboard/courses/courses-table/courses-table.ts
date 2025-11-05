a90fd084cd3f8a0a4b14097fd7149ca778ecfb99
Norka <norka1490@gmail.com>
2025-11-05 07:20:11 -0500

    tercer commit

--- src/app/featured/dashboard/courses/courses-table/courses-table.ts
+++ src/app/featured/dashboard/courses/courses-table/courses-table.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Course, courseColumns } from '../../../../core/services/courses/model/Course';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { CoursesService } from '../../../../core/services/courses/courses';
 
 @Component({
   selector: 'app-courses-table',
@@ -7,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrl: './courses-table.css',
 })
 export class CoursesTable {
+  displayedColumns: string[] = courseColumns;
+  dataSource = new MatTableDataSource<Course>([]);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(private courseService: CoursesService) {
+    this.courseService.courses$.subscribe((courses) => {
+      this.dataSource.data = courses;
+    });
+  }
+
+  ngOnInit() {
+    this.courseService.getCourses();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onDeleteCourse(id: number) {
+    this.courseService.deleteCourse(id);
+  }
+
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+
+  }
 }
