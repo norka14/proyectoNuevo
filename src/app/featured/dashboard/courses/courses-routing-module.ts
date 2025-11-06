cfcfaf9e126a1fa6c1546194a750691e615046a3
Norka <norka1490@gmail.com>
2025-11-05 20:54:56 -0500

    avanzando

--- src/app/featured/dashboard/courses/courses-routing-module.ts
+++ src/app/featured/dashboard/courses/courses-routing-module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
         path:'create',
         component: CoursesForm
       },
+       {
+        path:'edit/:id',
+        component: CoursesForm
+      }
     ]
   }
 ];
